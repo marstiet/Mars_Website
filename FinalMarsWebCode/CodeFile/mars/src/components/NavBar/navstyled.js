2654ce85ebd7eb47c367c3d72d9204905770d791
Chhayyaa <149094648+Chhayyaa@users.noreply.github.com>
2025-06-29 01:46:06 +0530

    Update navstyled.js

--- FinalMarsWebCode/CodeFile/mars/src/components/NavBar/navstyled.js
+++ FinalMarsWebCode/CodeFile/mars/src/components/NavBar/navstyled.js
@@ -58,7 +58,7 @@ export const MarkNameHolder = styled.div`
 
   @media screen and (max-width: 720px) {
     width: 200px;
-    border-top:80px;
+    border-top:90px solid #f16837;
     border-top-width: 60px;
     border-left-width: 25px;
     border-right-width: 25px;
