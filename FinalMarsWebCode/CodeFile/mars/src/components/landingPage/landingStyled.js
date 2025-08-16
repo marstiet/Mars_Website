dd24a69b53c256545d18d03c8f94df5a770a5a2a
Chhayyaa <149094648+Chhayyaa@users.noreply.github.com>
2025-08-17 00:21:24 +0530

    fucckkkk

--- FinalMarsWebCode/CodeFile/mars/src/components/landingPage/landingStyled.js
+++ FinalMarsWebCode/CodeFile/mars/src/components/landingPage/landingStyled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import NulshockBold from '../../assets/font/nulshock bd.ttf';
+import NulshockBold from '../../assets/font/Nulshock-Bold.woff';
 
 // Image Holders
 export const ImgHolder = styled.div`
