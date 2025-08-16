a8319e1973c8fee486ed5e4153a2003f7365c50e
Chhayyaa <149094648+Chhayyaa@users.noreply.github.com>
2025-08-16 23:20:17 +0530

    Update landingStyled.js

--- FinalMarsWebCode/CodeFile/mars/src/components/landingPage/landingStyled.js
+++ FinalMarsWebCode/CodeFile/mars/src/components/landingPage/landingStyled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import NulshockBold from '../../assest/font/nulshock bd.ttf';
+import NulshockBold from '../../assets/font/nulshock bd.ttf';
 
 // Image Holders
 export const ImgHolder = styled.div`
